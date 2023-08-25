2c50038baab3dba19dba47a8ca7020c63a2f4c4a
Alp <efe1998alp@hotmail.com>
2023-08-25 15:49:53 +0300

    .

--- src/Screens/CategoryDetail.js
+++ src/Screens/CategoryDetail.js
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import coffees from '../config/coffees';
 
 const CategoryDetail = ({ route }) => {
   const { id } = route.params;
@@ -7,6 +8,11 @@ const CategoryDetail = ({ route }) => {
   return (
     <View style = {{flex:1 , height:'100%' , justifyContent:'center' , alignItems:'center'}}>
       <Text style = {{color:'black'}}>{id}</Text>
+      {
+        coffees.filter((item) => item.id === id).map((item) => (
+          <Text>{item.name}</Text>
+        ))
+      }
     </View>
   )
 }
